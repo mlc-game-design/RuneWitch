1af43aa7928f346b6e163793c81a57b8402edf1b
Max Courneya <max@mlcgaming.com>
2019-01-23 16:27:15 -0600

    Created folder structure to properly file class groups by function. Added new classes for Dialogue engine

---
+++ RuneWitch/DialogueItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuneWitch
+{
+    public class DialogueItem
+    {
+        public List<string> Dialogues { get; protected set; }
+        private float CharacterTimer { get; set; }
+        private string CurrentText { get; set; }
+
+    }
+}
